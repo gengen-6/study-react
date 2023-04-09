6b5ab871c9f4e8d7a84b567e4d1898d3079a1f0d
gengen-6 <ukz7708yk@gmil.com>
2023-04-09 23:33:26 +0900

    ✨ Headerの追加

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Footer } from '@/components/Footer';
 import { Main } from '/components/Main';
-import Link from 'next/link';
+import { Header } from '@/components/Header';
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Link href={'/'}>index</Link>
+      <Header />
 
       <Main page="about" />
       <Footer />
